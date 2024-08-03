89084f556b78b624b1265829a3ab213249e4b37e
bssathwik <balabhadrasaisathwik@gmail.com>
2024-08-03 09:47:43 +0530

    ok

--- sampel.js
+++ sampel.js
@@ -2,6 +2,6 @@ document.getElementById("sai").innerHTML="sathwik1"
 
 document.getElementsByClassName("sai")[1].textContent="sathwik"
  
-const sat = document.createElement('names');
-sat.innerHTML="<i>sathwik</i>"
-document.body.appendChild(sat);
+const sat = document.createElement('names');    
+sat.innerHTML="<i> sathwik </i>"
+document.body.appendChild(sat); 
