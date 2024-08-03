1c90a7d955eacd52c8458843108907cf405071b9
bssathwik <balabhadrasaisathwik@gmail.com>
2024-08-03 09:46:36 +0530

    ok

---
+++ sampel.js
@@ -0,0 +1,7 @@
+document.getElementById("sai").innerHTML="sathwik1"
+
+document.getElementsByClassName("sai")[1].textContent="sathwik"
+ 
+const sat = document.createElement('names');
+sat.innerHTML="<i>sathwik</i>"
+document.body.appendChild(sat);
